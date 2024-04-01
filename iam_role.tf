3e1140d4fffd075c9e594708e0c5788158816554
sogwujiakwu <108236185+sogwujiakwu@users.noreply.github.com>
2024-04-01 18:36:16 -0400

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -121,6 +121,7 @@ resource "aws_iam_role" "ssm_role" {
         }
     ]
 })
+}
 resource "aws_iam_role_policy_attachment" "ssm-attach" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
