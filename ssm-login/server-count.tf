8ad670a4fa8c504298c83bfff9370151af1b6286
Ubuntu <ubuntu@ip-172-31-84-28.ec2.internal>
2024-04-02 00:52:37 +0000

    fixed commit issues

---
+++ ssm-login/server-count.tf
@@ -0,0 +1,41 @@
+/* terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+*/
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  count                  = 2
+  ami                    = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+
+  tags = {
+    Name = "WebServer-${count.index}"
+  }
+}
---
+++ ssm-login/vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "workman" {
+  cidr_block           = "10.16.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "workman"
+  }
+}
