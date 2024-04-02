56748d28443b33598404e885443e8072e5d2d640
Ubuntu <ubuntu@ip-172-31-84-28.ec2.internal>
2024-04-02 00:54:23 +0000

    modified server-count.tf

--- ssm-login/server-count.tf
+++ ssm-login/server-count.tf
@@ -31,7 +31,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   count                  = 2
   ami                    = data.aws_ami.ubuntu.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   instance_type          = "t2.micro"
   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
 
