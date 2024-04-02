a9785fa6c6e192b32415cba1b16fede65e379d3b
Ubuntu <ubuntu@ip-172-31-84-28.ec2.internal>
2024-04-02 01:06:21 +0000

    updated server-count.tf

--- ssm-login/server-count.tf
+++ ssm-login/server-count.tf
@@ -29,10 +29,10 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  count                  = 2
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  count                = 2
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
 
   tags = {
     Name = "WebServer-${count.index}"
