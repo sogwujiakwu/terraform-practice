ca00beda62b5daef7f04ce8f76450ee7f11fa207
Ubuntu <ubuntu@ip-172-31-84-28.ec2.internal>
2024-04-03 19:28:56 +0000

    added foreach/

---
+++ foreach/random.tf
@@ -0,0 +1,7 @@
+resource "random_pet" "server" {
+/*  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    ami_id = var.ami_id
+  }
+*/
+}
---
+++ foreach/server-foreach.tf
@@ -0,0 +1,40 @@
+/* terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+*/
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  for_each             = toset(["01", "03", "02"])
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  tags = {
+    Name = "WebServer-${each.value}"
+  }
+}
